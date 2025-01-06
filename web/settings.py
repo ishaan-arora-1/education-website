df514094e378b01e2c7be82d37b64ce72e0aa7cc
A1L13N <193832434+A1L13N@users.noreply.github.com>
2025-01-05 20:00:19 -0500

    Enhance .env file handling in settings.py with existence check and informative print statements

--- web/settings.py
+++ web/settings.py
@@ -9,7 +9,19 @@ SECRET_KEY = "django-insecure-5kyff0s@l_##j3jawec5@b%!^^e(j7v)ouj4b7q6kru#o#a)o3
 
 # Initialize environment variables
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR.parent, ".env"))
+
+# Define the path to the .env file
+env_file = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env"
+)
+
+# Check if the .env file exists
+if os.path.exists(env_file):
+    print(f"Using env file: {env_file}")
+    environ.Env.read_env(env_file)
+else:
+    print("No .env file found.")
+
 
 # print out the env file used
 print(f"Using env file: {env.path}")
