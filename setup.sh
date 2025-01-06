a77172d26c225cfe8d197acfb117d9e4617922f9
A1L13N <193832434+A1L13N@users.noreply.github.com>
2025-01-05 22:02:29 -0500

    Uncomment migration and collectstatic commands in setup.sh for proper deployment setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ if find /home/alphaonelabs99282llkb/web/requirements.txt -mmin -60 | grep -q .; 
     pip install -r requirements.txt
 fi
 # only run these if the migration folder changed
-#python manage.py migrate
+python manage.py migrate
 # only run this if there are new static files
-#python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
