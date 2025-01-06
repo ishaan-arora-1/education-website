fd303fcd0fc4b704369a962fa153445daedb12eb
A1L13N <193832434+A1L13N@users.noreply.github.com>
2025-01-05 23:28:07 -0500

    Update setup.sh to check for recent changes in requirements.txt every minute

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ cd /home/alphaonelabs99282llkb/web/
 git reset --hard HEAD
 git pull
 
-if find /home/alphaonelabs99282llkb/web/requirements.txt -mmin -60 | grep -q .; then
+if find /home/alphaonelabs99282llkb/web/requirements.txt -mmin -1 | grep -q .; then
     pip install -r requirements.txt
 fi
 python manage.py migrate
