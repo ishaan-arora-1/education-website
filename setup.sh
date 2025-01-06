973b8b90399a90b3aece6f57b9c2ceb01589ad36
A1L13N <193832434+A1L13N@users.noreply.github.com>
2025-01-05 21:27:36 -0500

    Optimize setup script to conditionally install requirements and manage migrations

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 cd /home/alphaonelabs99282llkb/web/
 git reset --hard HEAD
 git pull
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
+
+if find /home/alphaonelabs99282llkb/web/requirements.txt -mmin -60 | grep -q .; then
+    pip install -r requirements.txt
+fi
+# only run these if the migration folder changed
+#python manage.py migrate
+# only run this if there are new static files
+#python manage.py collectstatic --noinput
