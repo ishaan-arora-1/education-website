64808c766721440cc8a1e579af1c756bebb15264
ishaan-arora-1 <ishaana612@gmail.com>
2025-03-19 23:39:44 +0530

    removed unecessary migrations

--- web/migrations/0029_profile_avatar_eyes_profile_avatar_hair_and_more.py
+++ web/migrations/0029_profile_avatar_eyes_profile_avatar_hair_and_more.py
@@ -12,54 +12,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name="profile",
-            name="avatar_eyes",
-            field=models.CharField(
-                choices=[("round", "Round"), ("almond", "Almond"), ("narrow", "Narrow")], default="round", max_length=20
-            ),
-        ),
-        migrations.AddField(
-            model_name="profile",
-            name="avatar_hair",
-            field=models.CharField(
-                choices=[("short", "Short"), ("long", "Long"), ("curly", "Curly"), ("bald", "Bald")],
-                default="short",
-                max_length=20,
-            ),
-        ),
-        migrations.AddField(
-            model_name="profile",
-            name="avatar_hair_color",
-            field=models.CharField(
-                choices=[("black", "Black"), ("brown", "Brown"), ("blonde", "Blonde"), ("red", "Red")],
-                default="black",
-                max_length=20,
-            ),
-        ),
-        migrations.AddField(
-            model_name="profile",
-            name="avatar_skin_tone",
-            field=models.CharField(
-                choices=[("light", "Light"), ("medium", "Medium"), ("dark", "Dark")], default="medium", max_length=20
-            ),
-        ),
-        migrations.RemoveField(
-            model_name="profile",
-            name="avatar_eyes",
-        ),
-        migrations.RemoveField(
-            model_name="profile",
-            name="avatar_hair",
-        ),
-        migrations.RemoveField(
-            model_name="profile",
-            name="avatar_hair_color",
-        ),
-        migrations.RemoveField(
-            model_name="profile",
-            name="avatar_skin_tone",
-        ),
-        migrations.AddField(
-            model_name="profile",
             name="avatar_background",
             field=models.CharField(
                 choices=[
