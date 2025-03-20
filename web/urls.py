279256cbbd10ca3150fd5e359fc9f9a354879f79
10done <anubhavtandon6424@gmail.com>
2025-03-20 06:01:09 +0530

    Updated URLs

--- web/urls.py
+++ web/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import include, path
 
 from . import admin_views, views
-from .views import GoodsListingView, add_goods_to_cart, sales_analytics, sales_data, streak_detail, student_dashboard
+from .views import GoodsListingView, add_goods_to_cart, sales_analytics, sales_data, streak_detail
 
 # Non-prefixed URLs
 urlpatterns = [
@@ -14,7 +14,6 @@ urlpatterns = [
     path("captcha/", include("captcha.urls")),  # CAPTCHA URLs should not be language-prefixed
     path("certificate/<uuid:certificate_id>/", views.certificate_detail, name="certificate_detail"),
     path("certificate/generate/<int:enrollment_id>/", views.generate_certificate, name="generate_certificate"),
-    path("dashboard/", student_dashboard, name="student_dashboard_root"),
     path("streak/", streak_detail, name="streak_detail"),
 ]
 
