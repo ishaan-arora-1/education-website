3de8d084d777a80ae0869fe1185ac59871f5db3b
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-04-08 09:25:46 +0530

    Update views and base/social media dashboard templates (#487)
    * Breadcrumbs for Blog added
    
    * Added page that shows users who have their public profile set
    
    * page for scorecard and public users
    
    * public user scorecard requestec changes made
    
    * linked the scoreboard to navigation
    
    * X and twitter logo issue solved
    
    * Update views and base/social media dashboard templates suggested issues resolved
    
    * icon alignment corrected
    
    * social media dashboard link added
    
    * white coloured logo
    
    ---------
    
    Co-authored-by: A1L13N <193832434+A1L13N@users.noreply.github.com>

--- web/views.py
+++ web/views.py
@@ -6945,7 +6945,7 @@ def get_twitter_client():
     return tweepy.API(auth)
 
 
-@user_passes_test(social_media_manager_required)
+@user_passes_test(social_media_manager_required, login_url="/accounts/login/")
 def social_media_dashboard(request):
     # Fetch all posts that haven't been posted yet
     posts = ScheduledPost.objects.filter(posted=False).order_by("-id")
